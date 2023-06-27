3df64b8b43244ce6c0f20d3980397abe714dcabb
MICHAEL-STEWART0 <michaelstewart19971@gmail.com>
2023-06-26 21:53:07 -0500

    Refactor: refactored gameCell class numbers in index.html Refactor: moved behavior from gameRules to ticTacToe constructor

--- TicTacToeOOP/main.js
+++ TicTacToeOOP/main.js
@@ -1,44 +1,50 @@
 const playingField = document.querySelector(".gameBoard");
 const gameCells = document.querySelectorAll(".gameCell");
+
 const gameRules = {
   turn: 0, // x = 0, y = 1. x goes first.
   currentPlayerLetter: `x`, //default to x
   placedSuccesfully: false,
-  setPlayerLetter: function (playerTurn) {
-    if (playerTurn === 0) {
-      this.currentPlayerLetter = `x`.toUpperCase();
-    } else {
-      this.currentPlayerLetter = `o`.toUpperCase();
-    }
-  },
-  updateBoardGameCells: function (e, playerTurn) {
-    if (e.target.innerHTML === "") {
-      e.target.innerHTML = `${this.currentPlayerLetter}`;
-      this.placedSuccesfully = true;
-    } else {
-      alert(`cell not empty`);
-      this.placedSuccesfully = false;
-    }
-    if (this.placedSuccesfully === true) {
-      this.updatePlayerTurn(this.turn);
-    }
-    this.setPlayerLetter(this.turn);
-  },
-  updatePlayerTurn: function (playerTurn) {
-    if (playerTurn === 0) {
-      this.turn = ++playerTurn;
-    } else this.turn = 0;
-  },
 };
 
 function createGameBoard(gameRules, gameCells) {
   this.gameRules = gameRules;
   this.gameCells = gameCells;
   gameCells.forEach((el) => {
-    el.addEventListener("click", function (e) {
-      gameRules.updateBoardGameCells(e, gameRules.turn);
+    el.addEventListener("click", (e) => {
+      this.updateBoardGameCells(e, gameRules.turn);
     });
   });
+
+  this.setPlayerLetter = function (playerTurn) {
+    if (playerTurn === 0) {
+      this.gameRules.currentPlayerLetter = `x`.toUpperCase();
+    } else {
+      this.gameRules.currentPlayerLetter = `o`.toUpperCase();
+    }
+  };
+
+  this.updateBoardGameCells = function (e, playerTurn) {
+    if (e.target.innerHTML === "") {
+      e.target.innerHTML = `${this.gameRules.currentPlayerLetter}`;
+      this.gameRules.placedSuccesfully = true;
+    } else {
+      alert(`cell not empty`);
+      this.gameRules.placedSuccesfully = false;
+    }
+    if (this.gameRules.placedSuccesfully === true) {
+      this.updatePlayerTurn(this.gameRules.turn);
+    }
+    this.setPlayerLetter(this.gameRules.turn);
+  };
+
+  this.updatePlayerTurn = function (playerTurn) {
+    if (playerTurn === 0) {
+      this.gameRules.turn = ++playerTurn;
+    } else this.gameRules.turn = 0;
+  };
 }
 
 const ticTacToeGame = new createGameBoard(gameRules, gameCells);
+
+// TO DO, ADD CHECKWIN LOGIC. use nodelist to compare matrix values
