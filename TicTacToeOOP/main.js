2aba07ea4f85a0beb95d08c8fc948949f966b978
MICHAEL-STEWART0 <michaelstewart19971@gmail.com>
2023-06-27 14:43:52 -0500

    Add: win conditions to main.js, add win field to index.html, updated general styling of style.css

--- TicTacToeOOP/main.js
+++ TicTacToeOOP/main.js
@@ -1,41 +1,46 @@
 const playingField = document.querySelector(".gameBoard");
 const gameCells = document.querySelectorAll(".gameCell");
+const playerWinField = document.getElementById("playerWinField");
 
 const gameRules = {
   turn: 0, // x = 0, y = 1. x goes first.
-  currentPlayerLetter: `x`, //default to x
+  currentPlayerLetter: `X`, //default to x
   placedSuccesfully: false,
 };
 
 function createGameBoard(gameRules, gameCells) {
   this.gameRules = gameRules;
   this.gameCells = gameCells;
+
   gameCells.forEach((el) => {
     el.addEventListener("click", (e) => {
-      this.updateBoardGameCells(e, gameRules.turn);
+      this.updateBoardGameCells(e);
     });
   });
 
   this.setPlayerLetter = function (playerTurn) {
     if (playerTurn === 0) {
-      this.gameRules.currentPlayerLetter = `x`.toUpperCase();
+      this.gameRules.currentPlayerLetter = `X`;
     } else {
-      this.gameRules.currentPlayerLetter = `o`.toUpperCase();
+      this.gameRules.currentPlayerLetter = `O`;
     }
   };
 
-  this.updateBoardGameCells = function (e, playerTurn) {
+  this.updateBoardGameCells = function (e) {
+    if (playerWinField.textContent !== "") {
+      return;
+    }
     if (e.target.innerHTML === "") {
       e.target.innerHTML = `${this.gameRules.currentPlayerLetter}`;
       this.gameRules.placedSuccesfully = true;
     } else {
       alert(`cell not empty`);
-      this.gameRules.placedSuccesfully = false;
+      return;
     }
-    if (this.gameRules.placedSuccesfully === true) {
-      this.updatePlayerTurn(this.gameRules.turn);
-    }
+
+    this.updatePlayerTurn(this.gameRules.turn);
     this.setPlayerLetter(this.gameRules.turn);
+    this.checkWin();
   };
 
   this.updatePlayerTurn = function (playerTurn) {
@@ -43,8 +48,50 @@ function createGameBoard(gameRules, gameCells) {
       this.gameRules.turn = ++playerTurn;
     } else this.gameRules.turn = 0;
   };
+
+  this.checkWin = function () {
+    for (let i = 0; i < 3; i++) {
+      let x = "";
+      for (let j = 0; j < 3; j++) {
+        //horizontal check
+        x += gameCells.item(j + i * 3).textContent;
+      }
+      if (x === "xxx".toUpperCase() || x === "ooo".toUpperCase()) {
+        x === "xxx".toUpperCase()
+          ? (playerWinField.innerHTML = `X wins`)
+          : (playerWinField.innerHTML = `O wins`);
+      }
+      x = "";
+      for (let k = 0; k < 3; k++) {
+        //vertical check
+        x += gameCells.item(k * 3 + i).textContent;
+      }
+      if (x === "xxx".toUpperCase() || x === "ooo".toUpperCase()) {
+        x === "xxx".toUpperCase()
+          ? (playerWinField.innerHTML = `X wins`)
+          : (playerWinField.innerHTML = `O wins`);
+      }
+    }
+    let x = "";
+    for (let i = 0; i <= 8; i += 4) {
+      x += gameCells.item(i).textContent; // everything below is diagonal check
+    }
+    if (x === "xxx".toUpperCase() || x === "ooo".toUpperCase()) {
+      x === "xxx".toUpperCase()
+        ? (playerWinField.innerHTML = `X wins`)
+        : (playerWinField.innerHTML = `O wins`);
+    }
+    x = "";
+    for (let i = 2; i <= 6; i += 2) {
+      x += gameCells.item(i).textContent;
+    }
+    if (x === "xxx".toUpperCase() || x === "ooo".toUpperCase()) {
+      x === "xxx".toUpperCase()
+        ? (playerWinField.innerHTML = `X wins`)
+        : (playerWinField.innerHTML = `O wins`);
+    }
+    console.log(playerWinField.textContent);
+  };
 }
 
 const ticTacToeGame = new createGameBoard(gameRules, gameCells);
-
-// TO DO, ADD CHECKWIN LOGIC. use nodelist to compare matrix values
