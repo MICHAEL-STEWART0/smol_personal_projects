c9cf931d82827c40c476fd9ccfcf9ab5c9eaa5eb
MICHAEL-STEWART0 <michaelstewart19971@gmail.com>
2023-07-13 15:34:11 -0500

    Fully refactored all code to be more modular project done. Will do styling eventually

--- FindACocktail/js/main.js
+++ FindACocktail/js/main.js
@@ -1,51 +1,10 @@
 const displayFields = {
   drinkNameDisplayField: document.querySelector("#drinkDisplayField"),
-  imageDisplayField: document.querySelector("#drinkImgDisplayField"),
-  ingredientDisplayField: document.querySelector("#drinkIngredientList"),
-  instructionDisplayField: document.querySelector("#instructionsToMakeDrink"),
-
-  updateFieldWithNoResult() {
-    this.drinkNameDisplayField.style.color = "red";
-    this.drinkNameDisplayField.textContent = "Not Found";
-    this.imageDisplayField.style.display = "none";
-  },
-
-  setDrinkName(drink) {
-    this.drinkNameDisplayField.textContent = drink.strDrink;
-  },
-
-  updateFieldWithResult(data) {
-    let drink = data.drinks[0];
-    this.setDrinkName(drink);
-    this.imageDisplayField.setAttribute("src", `${drink.strDrinkThumb}`); //set photo src
-    this.imageDisplayField.style.width = "20vw";
-    this.imageDisplayField.style.borderRadius = "10px";
-    this.imageDisplayField.setAttribute(
-      //set alt
-      `alt`,
-      `A picture of a ${drink.strDrink}`
-    );
-    this.updateInstructions(data); //set instruction list items
-  },
-
-  updateFieldWithRandomResult(arrOfDrinks) {
-    let randomDrink = arrOfDrinks[getRandomNumber(0, arrOfDrinks.length - 1)];
-    console.log(randomDrink);
-    this.drinkNameDisplayField.textContent = randomDrink.strDrink;
-    this.imageDisplayField.setAttribute("src", `${randomDrink.strDrinkThumb}`);
-    this.imageDisplayField.setAttribute(
-      `alt`,
-      `A picture of a ${randomDrink.strDrink}`
-    );
-    this.updateInstructions(data); //set instruction list items
-  },
-
-  resetStyling() {
-    this.drinkNameDisplayField.style.color = "black";
-    this.drinkNameDisplayField.style.textAlign = "center";
-    this.imageDisplayField.style.display = null;
-    this.clearListItems();
-  },
+  drinkImageDisplayField: document.querySelector("#drinkImgDisplayField"),
+  drinkIngredientDisplayField: document.querySelector("#drinkIngredientList"),
+  drinkInstructionDisplayField: document.querySelector(
+    "#instructionsToMakeDrink"
+  ),
 
   startEventListners() {
     buttons.fetchUserDrinkButton.addEventListener("click", function (e) {
@@ -59,20 +18,73 @@ const displayFields = {
     });
   },
 
-  updateInstructions(data) {
-    let drink = data.drinks[0];
-    for (let i = 1; i < 15; i++) {
-      if (drink[`strIngredient` + i] === null) {
+  updateFieldsWithUserChoice(closestMatchedDrink) {
+    this.resetErrorStyling();
+    this.updateDrinkNameField(closestMatchedDrink);
+    this.updateDrinkImageField(closestMatchedDrink);
+    this.clearIngredientListItems();
+    this.clearInstructionField();
+    this.updateDrinkIngredientField(closestMatchedDrink);
+    this.updateDrinkInstructionField(closestMatchedDrink);
+  },
+
+  updateFieldsWithRandomChoice(closestMatchedDrink) {
+    this.resetErrorStyling();
+    this.updateDrinkNameField(closestMatchedDrink);
+    this.updateDrinkImageField(closestMatchedDrink);
+    this.clearIngredientListItems();
+    this.clearInstructionField();
+    this.updateDrinkIngredientField(closestMatchedDrink);
+    this.updateDrinkInstructionField(closestMatchedDrink);
+  },
+
+  updateDrinkNameField(closestMatchedDrink) {
+    this.drinkNameDisplayField.textContent = closestMatchedDrink.strDrink;
+  },
+
+  updateDrinkImageField(closestMatchedDrink) {
+    this.drinkImageDisplayField.src = closestMatchedDrink.strDrinkThumb;
+  },
+
+  updateDrinkIngredientField(closestMatchedDrink) {
+    for (let i = 1; i <= 15; i++) {
+      if (closestMatchedDrink[`strIngredient` + i.toString()] === null) {
         break;
       }
-      this.ingredientDisplayField.appendChild(
+      displayFields.drinkIngredientDisplayField.appendChild(
         document.createElement(`li`)
-      ).textContent = drink[`strIngredient` + i];
+      ).textContent = closestMatchedDrink[`strIngredient` + i.toString()];
     }
   },
 
-  clearListItems() {
-    this.ingredientDisplayField.innerHTML = "";
+  updateWithNoResult() {
+    this.drinkImageDisplayField.src = "";
+    this.drinkNameDisplayField.style.color = "red";
+    this.drinkNameDisplayField.textContent = "NotFound";
+    this.clearIngredientListItems();
+    this.clearInstructionField();
+  },
+
+  updateDrinkInstructionField(closestMatchedDrink) {
+    console.log(closestMatchedDrink.strInstructions.split(". "));
+
+    closestMatchedDrink.strInstructions.split(". ").forEach((sentance) => {
+      this.drinkInstructionDisplayField.appendChild(
+        document.createElement(`li`)
+      ).textContent = sentance;
+    });
+  },
+
+  resetErrorStyling() {
+    this.drinkNameDisplayField.style.color = "black";
+  },
+
+  clearIngredientListItems() {
+    this.drinkIngredientDisplayField.innerHTML = "";
+  },
+
+  clearInstructionField() {
+    this.drinkInstructionDisplayField.innerHTML = "";
   },
 };
 
@@ -88,33 +100,35 @@ const inputFields = {
 displayFields.startEventListners();
 
 async function fetchDrink(userGeneratedEvent) {
-  const userChoice = inputFields.drinkSearchField.value;
+  let userChoice = inputFields.drinkSearchField.value;
+
   const url =
     "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + userChoice;
 
   const response = await fetch(url);
   const data = await response.json();
-  const arrOfDrinks = data.drinks;
+  const drinks = data.drinks;
+  if (drinks === null) {
+    displayFields.updateWithNoResult();
+    return;
+  }
 
   if (
     userGeneratedEvent === "userDrinkButton" ||
     userGeneratedEvent === "Enter"
   ) {
-    /* if the search field is empty or the resonse 
-        has no drink update field with no result */
-    if (userChoice === "" || arrOfDrinks === null) {
-      displayFields.updateFieldWithNoResult();
-      displayFields.clearListItems();
+    // do something with user drink choice
+    const closestMatchedDrink = drinks[0];
+    if (userChoice === "") {
+      displayFields.updateWithNoResult();
       return;
-    } else if (arrOfDrinks !== null) {
-      /* if there is a present drink reset 
-          styling to from previous errors */
-      displayFields.resetStyling();
     }
-    displayFields.updateFieldWithResult(data);
+    displayFields.updateFieldsWithUserChoice(closestMatchedDrink);
   } else {
-    displayFields.resetStyling();
-    displayFields.updateFieldWithRandomResult(arrOfDrinks);
+    //do something with random drink
+    console.log(`random choice`, drinks);
+    const randomDrink = drinks[getRandomNumber(0, drinks.length - 1)];
+    displayFields.updateFieldsWithRandomChoice(randomDrink);
   }
 }
 
