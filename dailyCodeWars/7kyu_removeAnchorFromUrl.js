b99a7191dd027870358f05ca95905ddf3ad6da10
MICHAEL-STEWART0 <michaelstewart19971@gmail.com>
2023-07-25 22:56:15 -0500

    Add: Daily cc 8kyu

---
+++ dailyCodeWars/7kyu_removeAnchorFromUrl.js
@@ -0,0 +1,13 @@
+/*Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+Examples
+"www.codewars.com#about" --> "www.codewars.com"
+"www.codewars.com?page=1" -->"www.codewars.com?page=1" */
+// solution
+function removeUrlAnchor(url) {
+  return url.includes("#")
+    ? url.split("").splice(0, url.indexOf("#")).join("")
+    : url.includes("?")
+    ? url.split("").splice(0, url.indexOf("?")).join("")
+    : url;
+}
