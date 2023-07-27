ad0c3ef5b8a4361c588890dc441b5631d6668f16
Michael Stewart <121202837+MICHAEL-STEWART0@users.noreply.github.com>
2023-07-26 21:40:14 -0500

    Calculator app (#3)
    * init: bones for calculator app
    
    * Update: finished styling of calculator, Add: class names and ids to buttons
    
    * Refactor: Index.html updated class names and span temp text, Add: bones to main.js, Bugfix: reduced calc container size to prevent sidescrolling
    
    * Add: daily cc 8kyu
    
    * Add: daily CC 8kyu
    
    * Add: daily cc 8kyu

---
+++ calculator/js/main.js
@@ -0,0 +1,38 @@
+// this is still a WIP
+const buttons = {
+  primaryButton: document.querySelectorAll(".primaryButton"),
+  secondaryButton: document.querySelectorAll(".secondaryButton"),
+  clearButton: document.querySelector("#clearButton"),
+  equalButton: document.querySelector("#equalButton"),
+  ancillaryButton: document.querySelectorAll(".ancilliaryButton"),
+};
+
+const DisplayFields = {
+  resultField: document.querySelector(".calculator-result"),
+  userEnteredDataField: document.querySelector(
+    ".calculator-userEnteredData--displayField"
+  ),
+};
+
+// handles firing up the event listeners for all of the buttons
+for (let button in buttons) {
+  if (buttons[button].length !== undefined) {
+    buttons[button].forEach((el) =>
+      el.addEventListener("click", function (e) {
+        console.log(e.target.innerHTML); //TODO: pass target into function that handles data
+      })
+    );
+  } else {
+    buttons[button].addEventListener("click", function (e) {
+      console.log(e.target.innerHTML); //TODO: pass target into function that handles data
+    });
+  }
+}
+
+// need to handle multiple operators, every time an operator is used add it to a queue and use it then remove it from the array 
+
+// handle operator conversion using an iife Function() or eval() --note eval not recommended. better to use a function constructor and pass the body of the function and immediately invoke it to recieve a value
+
+// Function(`return ${document.querySelector('.calculator-userEnteredData--displayField').innerHTML})()
+
+// permited values[0,1,2,3,4,5,6,8,9]
