64ad4ddb15ef928de186606370be3ed584a1d718
MICHAEL-STEWART0 <michaelstewart19971@gmail.com>
2023-08-02 00:29:30 -0500

    daily cc challenge 8kyu

--- dailyCodeWars/8kyu_trainingJS.js
+++ dailyCodeWars/8kyu_trainingJS.js
@@ -1,4 +1,4 @@
-/*Coding in function trueOrFalse, function accept 1 parameters:val, try to use the conditional statement if...else, if val value is false (val==false or it can convert to false), should return a string "false", if not, return a string "true". */
+/*Coding in function trueOrFalse, function accept 1 parameters:val, try to use the conditional statement if...else, if val value is false (val==false or it can convert to false), should return a string "false", if not, return a string "true".. */
 
 //solution
 function trueOrFalse(val) {
