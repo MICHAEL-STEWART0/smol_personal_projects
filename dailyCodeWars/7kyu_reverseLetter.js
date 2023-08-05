1e591703d19495201cff1bc3af76982200a3000a
MICHAEL-STEWART0 <michaelstewart19971@gmail.com>
2023-08-04 22:15:04 -0500

    Add: daily code challenge 7kyu

---
+++ dailyCodeWars/7kyu_reverseLetter.js
@@ -0,0 +1,22 @@
+/*Task
+Given a string str, reverse it and omit all non-alphabetic characters.
+
+Example
+For str = "krishan", the output should be "nahsirk".
+
+For str = "ultr53o?n", the output should be "nortlu".
+
+Input/Output
+[input] string str
+A string consists of lowercase latin letters, digits and symbols.
+
+[output] a string*/
+//solution
+function reverseLetter(str) {
+  let filteredString = str
+    .split("")
+    .filter((char) => `abcdefghijklmnopqrstuvwxyz`.split("").includes(char))
+    .join("");
+
+  return filteredString.split("").reverse().join("");
+}
